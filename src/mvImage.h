45c8867d47ff986bddd8a48bc6490559302b87d9
tsgouros <tsgouros@gmail.com>
2016-10-26 22:42:05 -0400

    beginning to shape it; untested

--- src/mvImage.h
+++ src/mvImage.h
@@ -1,6 +1,49 @@
 // my first program in C++
 #include <iostream>
+#include <string>
 
+#include "MVR.h"
+
+#if defined(WIN32)
+#define NOMINMAX
+#include <windows.h>
+#include <GL/gl.h>
+#include <gl/GLU.h>
+#elif defined(__APPLE__)
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/glu.h>
+#else
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
+
+#define TWOPI 6.283185
+
+// An object to encapsulate a lot of working information about the
+// running program: viewer position, collection of mvImage objects to
+// display, and so on.
+class mvImageApp {
+ protected:
+  VRMain *_vrMain;
+  bool _quit;
+  double _horizAngle, _vertAngle, _radius, _incAngle;
+
+ public:
+  mvImageApp(int argc, char** argv);
+  virtual ~mvImage();
+
+  virtual void onVREvent(const std::string &eventName, VRDataIndex *eventData);
+  virtual void onVRRenderContext(VRDataIndex *renderState, 
+				 VRDisplayNode *callingNode);
+  virtual void onVRRenderScene(VRDataIndex *renderState, 
+			       VRDisplayNode *callingNode);
+
+  void run() { while (!_quit) { _vrMain->mainloop(); } };
+};
+
+// A class to hold an image object.  This is a 3D object located in space
+// somewhere, textured with an input image read from a file.
 class mvImage {
  private:
   int imageData;
@@ -11,4 +54,5 @@ class mvImage {
 
   int getImage() { return imageData; };
 };
+
 

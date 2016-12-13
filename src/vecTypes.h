2badb5f9de9f14918316b5280c6370b98240741b
tsgouros <tsgouros@gmail.com>
2016-12-13 07:08:12 -0500

    typedefs for vector types

---
+++ src/vecTypes.h
@@ -0,0 +1,17 @@
+#ifndef VECTYPES_H
+#define VECTYPES_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+
+typedef glm::vec2 MVec2;
+typedef glm::vec3 MVec3;
+typedef glm::vec4 MVec4;
+typedef glm::mat4 MMat4;
+typedef glm::mat3 MMat3;
+typedef glm::quat MQuat;
+
+#endif // vectypes
